0e48bbe5ceadcbdd33456d820d8d19c1bd9ea755
kelen51215 <58721349+kelen51215@users.noreply.github.com>
2023-03-03 00:49:19 +0800

    add the hyprland config

--- config/waybar/modules/datetime.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo "$(date '+%A %d %B, %H:%M') "
--- config/waybar/modules/mem.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mem(){
-	mem=`free | awk '/Mem/ {printf "%d Mb", $3 / 1024.0, $2 / 1024.0 }'`
-	sleep 3
-	echo  $mem
-}
-mem
